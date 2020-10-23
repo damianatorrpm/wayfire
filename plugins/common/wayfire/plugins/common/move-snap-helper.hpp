7a617f0debff63e032faa6e5e7218c6ffe4c4109
Damian Ivanov <damianatorrpm@gmail.com>
2020-10-23 08:51:53 +0300

    fixup

--- plugins/common/wayfire/plugins/common/move-snap-helper.hpp
+++ plugins/common/wayfire/plugins/common/move-snap-helper.hpp
@@ -147,13 +147,19 @@ class move_snap_helper_t : public wf::custom_data_t
          */
         if (view->fullscreen)
         {
-            wf::output_t *output   = view->get_output();
+            wf::output_t *output   = wf::get_core().get_active_output();
+            wf::geometry_t output_geometry = output->get_relative_geometry();
             wf::point_t current_ws = output->workspace;
-            wf::point_t target_ws  =
+            wf::point_t target_ws;
+            target_ws.x =
                 floor(
-                    (last_grabbing_position.x / view->get_output_geometry().width) +
+                    (last_grabbing_position.x / output_geometry.width) +
                     current_ws.x);
-            view->fullscreen_request(view->get_output(), true, target_ws);
+            target_ws.y =
+                floor(
+                    (last_grabbing_position.y / output_geometry.height) +
+                    current_ws.y);                    
+            view->fullscreen_request(output, true, target_ws);
         }
     }
 
